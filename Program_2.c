1fb6a5f3e77b9b1207942c9fa175ea494b9b0431
shama08134 <>
2021-05-27 14:01:13 +0000

    copy of elements from one array into another array

--- Program_2.c
+++ Program_2.c
@@ -19,15 +19,32 @@ int main()
    //Read the array elements using for loop and scanf statment
    //Copy the elements of array 'a'[first array] to  array 'b'[second array] using for loop
    //Print the both array elements in using printf statement in the above specified format
-   int arr1[100], arr[100];
+   int arr1[100], arr2[100];
    int i, n;
    printf("\n\nCopy the elements one array into another array :\n");
    printf("----------------------------------------------------\n");
    printf("Input the number of elements to be stored in the array :");
    scanf("%d",&n);
    printf("Input %d elements in the array :\n",n);
-   for(i=0;i<n;i++;)
+   for(i=0;i<n;i++)
    {
-     printf("elementd")
+     printf("element - %d : ",i);
+     scanf("%d",&arr1[i]);
    } 
+   for(i=0;i<=n;i++)
+   {
+     arr2[i]=arr1[i];
+   }
+   printf("\n The elements stored in first array are :\n");
+   for(i=0;i<n;i++)
+   {
+     printf("%5d", arr1[i]);
+   }
+   printf("\n\n The elements copied into the second array are :\n");
+   for(i=0;i<n;i++)
+   {
+     printf("%5d", arr2[i]);
+   }
+   printf("\n\n");
+   return 0;
 }
